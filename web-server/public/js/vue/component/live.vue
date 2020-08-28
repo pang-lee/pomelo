d21b872521c01b36ec1da9a152f17ec2b24f75d4
Shih-Pnag <leebond88@yahoo.com.tw>
2020-08-28 13:31:10 +0800

    add

--- web-server/public/js/vue/component/live.vue
+++ web-server/public/js/vue/component/live.vue
@@ -1,12 +1,32 @@
 <template>
-    <div>
-        foo
-    </div>
+    <b-container fluid class="mt-2">
+        <b-row>
+            <b-col>
+                <b-card border-variant="dark" class="text-center" title="Live">
+                    <video-player class="vjs-custom-skin" :options="playerOptions">
+                </b-card>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
 module.exports ={
-
+    data() {
+        return {
+            playerOptions: {
+                height: '360',
+                sources: [{
+                    type: "rtmp/mp4",
+                    src: "rtmp://184.72.239.149/vod/&mp4:BigBuckBunny_115k.mov"
+            }],
+            techOrder: ['flash'],
+            autoplay: false,
+            controls: true,
+            poster: "https://surmon-china.github.io/vue-quill-editor/static/images/surmon-9.jpg"
+        }
+      }
+    }
 }
 </script>
 
--- web-server/public/js/vue/vue.js
+++ web-server/public/js/vue/vue.js
@@ -2,6 +2,7 @@ const url = 'url:./js/vue/component/'
 
 document.addEventListener('DOMContentLoaded', () => {
   Vue.use(httpVueLoader)
+  Vue.use(window.VueVideoPlayer)
   new Vue({
     components: {
       'navbar': url + 'navbar.vue'
