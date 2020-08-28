7461d62ccdc85c0cf901923d9621dd33be4f2f23
Shih-Pnag <leebond88@yahoo.com.tw>
2020-08-28 11:02:32 +0800

    add

--- web-server/public/js/vue/router.js
+++ web-server/public/js/vue/router.js
@@ -5,6 +5,6 @@ const router = new VueRouter({
     { path: '/login', name: 'login', component: httpVueLoader('./js/vue/component/login.vue')},
     { path: '/live', name: 'live', component: httpVueLoader('./js/vue/component/live.vue') },
     { path: '/profile', name: 'profile', component: httpVueLoader('./js/vue/component/profile.vue') },
-    { path: '/:id', name: 'disqus', component: httpVueLoader('./js/vue/component/disqus.vue') }
+    { path: '/comment/:id', name: 'disqus', component: httpVueLoader('./js/vue/component/disqus.vue') }
   ]
 })
