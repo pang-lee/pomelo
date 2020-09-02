3370f436cc746565640c5ec9240218f32e48ae89
Shih-Pnag <leebond88@yahoo.com.tw>
2020-09-02 16:27:51 +0800

    add

--- web-server/public/js/vue/vue.js
+++ web-server/public/js/vue/vue.js
@@ -1,13 +1,11 @@
 const url = 'url:./js/vue/component/'
 
-document.addEventListener('DOMContentLoaded', () => {
-  Vue.use(httpVueLoader)
-  Vue.use(window.VueVideoPlayer)
-  new Vue({
-    components: {
-      'navbar': url + 'navbar.vue'
-    },
-    router,
-    store
-  }).$mount('#app')
-})
+Vue.use(httpVueLoader)
+Vue.use(window.VueVideoPlayer)
+new Vue({
+  components: {
+    'navbar': url + 'navbar.vue'
+  },
+  router,
+  store
+}).$mount('#app')
