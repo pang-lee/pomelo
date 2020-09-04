33e0646df798015fbe1cd0feae291aee2a1021aa
Shih-Pnag <leebond88@yahoo.com.tw>
2020-09-04 22:11:09 +0800

    add

--- game-server/app.js
+++ game-server/app.js
@@ -1,28 +1,42 @@
-var pomelo = require('pomelo');
+const pomelo = require('pomelo')
+const routeUtil = require('./app/utils/routeUtil.js')
 
 /**
  * Init app for client.
  */
-var app = pomelo.createApp();
-app.set('name', 'hello');
+const app = pomelo.createApp()
+app.set('name', 'hello')
 
 // app configuration
-app.configure('production|development', 'connector', function(){
-  app.set('connectorConfig',
-    {
-      connector : pomelo.connectors.sioconnector,
-      // 'websocket', 'polling-xhr', 'polling-jsonp', 'polling'
-      transports : ['websocket', 'polling'],
-      heartbeats : true,
-      closeTimeout : 60 * 1000,
-      heartbeatTimeout : 60 * 1000,
-      heartbeatInterval : 25 * 1000
-    });
-});
+app.configure('production|development', function(){
+  app.route('chat', routeUtil.chat)
+  app.set('connectorConfig',{
+    connector : pomelo.connectors.sioconnector,
+    // 'websocket', 'polling-xhr', 'polling-jsonp', 'polling'
+    transports : ['websocket', 'polling'],
+    heartbeats : true,
+    closeTimeout : 60 * 1000,
+    heartbeatTimeout : 60 * 1000,
+    heartbeatInterval : 25 * 1000
+  })
+  app.filter(pomelo.timeout())
+})
+
+// app.configure('production|development', 'connector', function(){
+//   app.set('connectorConfig',{
+//     connector : pomelo.connectors.sioconnector,
+//     // 'websocket', 'polling-xhr', 'polling-jsonp', 'polling'
+//     transports : ['websocket', 'polling'],
+//     heartbeats : true,
+//     closeTimeout : 60 * 1000,
+//     heartbeatTimeout : 60 * 1000,
+//     heartbeatInterval : 25 * 1000
+//   })
+// })
 
 // start app
-app.start();
+app.start()
 
 process.on('uncaughtException', function (err) {
-  console.error(' Caught exception: ' + err.stack);
-});
+  console.error(' Caught exception: ' + err.stack)
+})
