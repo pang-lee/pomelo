eed3f21e2357f50834b5fd557df654c026879c96
Shih-Pnag <leebond88@yahoo.com.tw>
2020-09-11 21:15:01 +0800

    add

--- web-server/public/src/components/game/driving.vue
+++ web-server/public/src/components/game/driving.vue
@@ -12,16 +12,33 @@ module.exports = {
     // Create the scene space
     let scene = new BABYLON.Scene(engine)
 
-    // Add a camera to the scene and attach it to the canvas
-    let camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), scene)
-    camera.attachControl(canvas, true)
+    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 
-    // Add lights to the scene
-    let light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene)
-    let light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene)
+    // This targets the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
 
-    // Add and manipulate meshes in the scene
-    let sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {}, scene)
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+
+    // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
+    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+
+    // Move the sphere upward 1/2 its height
+    sphere.position.y = 2;
+
+    // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
+    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+	
+	  scene.enablePhysics();
+	
+	  sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
+	  ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
 
     
     engine.runRenderLoop(() => {
