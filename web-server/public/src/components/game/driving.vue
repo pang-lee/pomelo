d3bd76952278388dd80b3f71d9cfffbf12e5b903
Shih-Pnag <leebond88@yahoo.com.tw>
2020-09-12 14:45:45 +0800

    add

--- web-server/public/src/components/game/driving.vue
+++ web-server/public/src/components/game/driving.vue
@@ -11,36 +11,39 @@ module.exports = {
     const engine = new BABYLON.Engine(canvas, true)
     // Create the scene space
     let scene = new BABYLON.Scene(engine)
-
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
-    // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
-
-    // This attaches the camera to the canvas
+   var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2,  Math.PI / 4, 5, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
-
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
-
-    // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 2;
-
-    // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
 	
-	  scene.enablePhysics();
+	//Light direction is up and left
+	var light = new BABYLON.HemisphericLight("hemiLight", new BABYLON.Vector3(-1, 1, 0), scene);
+	light.diffuse = new BABYLON.Color3(1, 0, 0);
+	light.specular = new BABYLON.Color3(0, 1, 0);
+	light.groundColor = new BABYLON.Color3(0, 1, 0);
 	
-	  sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
-	  ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+	var grass0 = new BABYLON.StandardMaterial("grass0", scene);
+	grass0.diffuseTexture = new BABYLON.Texture(require("./drive/grass.jpg"), scene);
+	
+	var grass1 = new BABYLON.StandardMaterial("grass1", scene);
+	grass1.emissiveTexture = new BABYLON.Texture(require("./drive/grass_grass_0131_01.jpg"), scene);
+	
+	var grass2 = new BABYLON.StandardMaterial("grass2", scene);
+	grass2.ambientTexture = new BABYLON.Texture(require("./drive/sky.jpg"), scene);
+	grass2.diffuseColor = new BABYLON.Color3(1, 0, 0);
+	
+	//diffuse texture
+	var sphere0 = BABYLON.MeshBuilder.CreateSphere("sphere0", {}, scene);
+	sphere0.position.x = -1.5;
+	sphere0.material = grass0;	
+	
+	//emissive texture 
+	var sphere1 = BABYLON.MeshBuilder.CreateSphere("sphere1", {}, scene);
+	sphere1.material = grass1;
+	
+	//ambient texture and diffuse color
+	var sphere2 = BABYLON.MeshBuilder.CreateSphere("sphere2", {}, scene);
+	sphere2.material = grass2;
+	sphere2.position.x = 1.5;		
 
-    
     engine.runRenderLoop(() => {
       scene.render()
     })
